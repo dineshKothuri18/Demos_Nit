b8ca542de5a50b783836362770bea6a0a26fedd0
nithisha <nithisre@amazon.com>
2022-07-24 15:52:43 -0500

    addes images and multiple contacts

--- DemoClass/CustomTableViewCell_Demo/CustomTVCellDemo/CustomTVCellDemo/ContactsVC.swift
+++ DemoClass/CustomTableViewCell_Demo/CustomTVCellDemo/CustomTVCellDemo/ContactsVC.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ContactsVC: UIViewController {
+    struct Sections{
+        var title: String
+        var rows: [Contacts]
+        }
+    struct Contacts {
+        var name: String
+        var phoneNmber: String
+        var email: String
+        var image: String
+        }
+    var tableViewModel = [Sections]()
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -16,35 +27,51 @@ class ContactsVC: UIViewController {
         // Do any additional setup after loading the view.
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        let c1 = Contacts.init(name: "name1", phoneNmber: "123", email: "x@mail.com", image: "image1")
+        let c2 = Contacts.init(name: "name2", phoneNmber: "321", email: "p@mail.com",image: "image2")
+        let c3 = Contacts.init(name: "name3", phoneNmber: "231", email: "y@mail.com",image: "image3")
+        let c4 = Contacts.init(name: "name4", phoneNmber: "312", email: "z@mail.com", image: "image4")
         
+        let c5 = Contacts.init(name: "name1", phoneNmber: "123", email: "x@mail.com", image: "image5")
+        let c6 = Contacts.init(name: "name2", phoneNmber: "321", email: "p@mail.com", image: "image6")
+        let c7 = Contacts.init(name: "name3", phoneNmber: "231", email: "y@mail.com", image: "image7")
+        
+        let c8 = Contacts.init(name: "name1", phoneNmber: "123", email: "x@mail.com", image: "image8")
+        let c9 = Contacts.init(name: "name2", phoneNmber: "321", email: "p@mail.com",image: "image9")
+        let c10 = Contacts.init(name: "name3", phoneNmber: "231", email: "y@mail.com", image: "image10")
+        let c11 = Contacts.init(name: "name4", phoneNmber: "312", email: "z@mail.com", image: "image11")
+        let c12 = Contacts.init(name: "name4", phoneNmber: "312", email: "z@mail.com",image:"image12")
+
+        
+        
+        
+        let section1 = Sections.init(title: "A",rows: [c1,c2,c3,c4])
+        let section2 = Sections.init(title: "B",rows: [c5,c6,c7])
+        let section3 = Sections.init(title: "C", rows: [c8,c9,c10,c11,c12])
+        
+        tableViewModel=[section1,section2,section3]
         
         let nib = UINib.init(nibName: "ContactTVCell", bundle: nil)
         self.tableView.register(nib, forCellReuseIdentifier: String.init(describing: ContactTVCell.self))
-    }
-    
+        view.addSubview(tableView)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
 
 extension ContactsVC : UITableViewDelegate,UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return  tableViewModel.count   }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
+        return tableViewModel[section].rows.count
+}
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTVCell") as! ContactTVCell
-        cell.emailLable.text = "Nitisha@gmail.com"
-        cell.nameLable.text = "Nitisha"
-        cell.phoneNumLable.text = "092179862387"
-        cell.profileImageView.image = UIImage.init(named: "goImage")
+        cell.nameLable.text = tableViewModel[indexPath.section].rows[indexPath.row].name
+        cell.phoneNumLable.text = tableViewModel[indexPath.section].rows[indexPath.row].phoneNmber
+        cell.emailLable.text = tableViewModel[indexPath.section].rows[indexPath.row].email
+        cell.profileImageView.image = UIImage.init(named: tableViewModel[indexPath.section].rows[indexPath.row].image)
         return cell
     }
     
